bc9c18bf42edfca3be2b1535a252856c89d9272b
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2018-02-26 18:20:43 +0100

    Initial commit
    Update gemspec
    
    Initial work to get an auth token
    
    Fix
    
    Fix indenting on yaml
    
    Remove useless test
    
    Supports codeclimate
    
    Remove some tests
    
    Run coverage on travis
    
    Fix yaml
    
    Fix a bunch of rubocop offences
    
    Various updates
    
    Support refreshing a token
    
    Add listing endpoints
    
    Update README
    
    Ignore gem files
    
    Specs for listing

---
+++ spec/lib/airbnb_api/service/listing_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe AirbnbApi::Service::Listing do
+  let(:client) { AirbnbApi::Client.new(id: '1234', secret: 'topsecret', oauth_token: 'awesome_secret') }
+  describe '#find' do
+    it 'should respond to .find' do
+      expect(described_class.new(client)).to respond_to(:find)
+    end
+
+    it 'should make a request to /listings/1' do
+      stub = stub_request(:get, 'https://api.airbnb.com/v2/listings/1')
+      described_class.new(client).find(1)
+      expect(stub).to have_been_requested
+    end
+  end
+
+  describe '#create' do
+    it 'makes a POST request to create listing' do
+      stub = stub_request(:post, 'https://api.airbnb.com/v2/listings')
+             .with(body: { name: 'listing name' })
+             .with(headers: { 'X-Airbnb-Oauth-Token' => /./ })
+             .to_return(body: response_fixture_for(:listing))
+      expect(c= client.listings.create(name: 'listing name')).to be_instance_of(AirbnbApi::Resource::Listing)
+      expect(stub).to have_been_requested
+      puts c.name
+    end
+  end
+end
